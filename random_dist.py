e778164f9e736a3abcc9f0a92fa02d993242abfd
toms <toms@workonline.africa>
2021-11-12 20:55:31 +0200

    used main() and added graph function

--- random_dist.py
+++ random_dist.py
@@ -1,23 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import random
 import matplotlib.pyplot as plt
 
 rangeofnum = int(input("Enter Range: "))
 qty = int(input("Enter Quantity: "))
-qtyfixed = qty
 distribution = [0]*rangeofnum
 
-while qty > 0:
-    n = random.randint(0,(rangeofnum-1))
-    distribution[n]=distribution[n]+1
-    qty = qty-1
+def main():
+    q = qty
+    while q > 0:
+        n = random.randint(0,(rangeofnum-1))
+        distribution[n]=distribution[n]+1
+        q = q-1
 
-x = [*range(0,rangeofnum)]
-y = distribution
+    x = [*range(0,rangeofnum)]
+    y = distribution
 
-plt.plot(x, y)
-plt.ylim(0,((qtyfixed/rangeofnum)*1.2))
-plt.xlabel("Range")
-plt.ylabel("Frequency")
-plt.title(f"Random Distribution: Range: {rangeofnum} Samples: {qtyfixed}")
-plt.savefig(f"./rendered_outcomes/plot_{rangeofnum}_{qtyfixed}.jpg")
-plt.show()
+    plotmygraph(x,y)
+
+def plotmygraph(x,y):
+    plt.plot(x, y)
+    plt.ylim(0,((qty/rangeofnum)*2))
+    plt.xlabel("Range")
+    plt.ylabel("Frequency")
+    plt.title(f"Random Distribution: Range: {rangeofnum} Samples: {qty}")
+    plt.savefig(f"./rendered_outcomes/plot_{rangeofnum}_{qty}.jpg")
+    plt.show()
+
+if __name__ == '__main__':
+    main()
