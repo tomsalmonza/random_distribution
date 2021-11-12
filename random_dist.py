0fa602b4f82729eaeece8311b336874a9dd12c9f
toms <toms@workonline.africa>
2021-11-12 16:30:13 +0200

    removed superfluous print statements

--- random_dist.py
+++ random_dist.py
@@ -10,14 +10,10 @@ while qty > 0:
     n = random.randint(0,(rangeofnum-1))
     distribution[n]=distribution[n]+1
     qty = qty-1
-    print(qty)
 
 x = [*range(0,rangeofnum)]
-print(x)
 y = distribution
-print(y)
-print(rangeofnum)
-print(qtyfixed/rangeofnum)
+
 plt.plot(x, y)
 plt.ylim(0,((qtyfixed/rangeofnum)*1.2))
 plt.xlabel("Range")
