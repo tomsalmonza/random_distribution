d6306165354ad6adec155ee0d69fa18d5a947610
toms <toms@workonline.africa>
2021-11-13 20:45:52 +0200

    added interactive functionality

--- random_dist.py
+++ random_dist.py
@@ -1,26 +1,58 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import time
 import random
 import matplotlib.pyplot as plt
 
+inc = input("Interactive Graph? y/n: ")
 rangeofnum = int(input("Enter Range: "))
-qty = int(input("Enter Quantity: "))
+qty = int(input("Enter Samples: "))
+if inc == "y":
+    smp_refresh = int(input("Samples per refresh?:"))
 distribution = [0]*rangeofnum
 
 def main():
-    q = qty
-    while q > 0:
-        n = random.randint(0,(rangeofnum-1))
-        distribution[n]=distribution[n]+1
-        q = q-1
-
     x = [*range(0,rangeofnum)]
     y = distribution
 
-    plotmygraph(x,y)
+    if inc == "n":
+        q = qty
+        while q > 0:
+            n = random.randint(0,(rangeofnum-1))
+            distribution[n]=distribution[n]+1
+            q = q-1
+        x = [*range(0,rangeofnum)]
+        plotmygraph(x,y)
+
+    elif inc == "y":
+        plt.ion()
+        figure, ax = plt.subplots(figsize=(10, 8))
+        line1, = ax.plot(x, y)
+        plt.xlabel("Range")
+        plt.ylabel("Frequency")
+        plt.ylim(0,((qty/rangeofnum)*2))
+        q = qty
+        count = 0
+        while q > 0:
+            n = random.randint(0,(rangeofnum-1))
+            distribution[n]=distribution[n]+1
+            q = q-1
+            count = count+1
+            y = distribution
+            if count%smp_refresh == 0:
+                line1.set_xdata(x)
+                line1.set_ydata(y)
+                plt.title(f"Random Distribution: Range: {rangeofnum} Samples: {qty-q} / {qty}")
+                figure.canvas.draw_idle()
+                figure.canvas.flush_events()
+        plt.ioff()
+        plotmygraph(x,y)
+    else:
+        pass
 
 def plotmygraph(x,y):
+
     plt.plot(x, y)
     plt.ylim(0,((qty/rangeofnum)*2))
     plt.xlabel("Range")
